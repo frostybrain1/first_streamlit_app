3dc66a531762bc69f0d9a5d6fbe911dd9dd9f096
frostybrain1 <brian.forster@tivityhealth.com>
2023-04-27 15:37:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-streamlit.text("What fruit would you like to add?")
-fruit_choice_2 = streamlit.text_input('Jackfruit')
+
+fruit_choice_2 = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding', fruit_choice_2)
