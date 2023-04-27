d8be0c771b3baa375d51157404ca90e54326e977
frostybrain1 <brian.forster@tivityhealth.com>
2023-04-27 16:00:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,14 +24,17 @@ streamlit.dataframe(fruits_to_show)
 
 # Display api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+ fruit_choice = streamlit.text_input('What fruit would you like information about?')
+ if not fruit_choice:
+   streamlit.error("Please select a fruit to get information.")
+ else:
+  streamlit.write('The user entered ', fruit_choice)
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+except URLError as e:
+ streamlit.error()
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-# pretty json
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# put it in a datatable
 streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
